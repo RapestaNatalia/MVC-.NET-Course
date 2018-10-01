028e6bbb63474199140390655f41e27400629f65
Natalia <nd.rapesta@gmail.com>
2018-10-01 17:28:38 -0300

    App with migration

--- Vidly/App_Start/RouteConfig.cs
+++ Vidly/App_Start/RouteConfig.cs
@@ -12,7 +12,13 @@ namespace Vidly
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+            routes.MapRoute(
+                "MoviesByReleaseDate",
+                "Movies/Released/{year}/{month}",
+                new { controller = "Movies", action = "ByReleaseDate"},
+                //new { year=@"\d{4}",month=@"\d{2}"}
+                new { year = @"2015|2016", month = @"\d{2}" }
+            );
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
